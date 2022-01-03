180100689d4f809ba7687168479122cc5a333be5
Dirk Arnez <smalldirkalex@gmail.com>
2022-01-03 18:02:51 +0800

    - init, wip

---
+++ local-build.cmd
@@ -0,0 +1,12 @@
+REM run as Administrator
+@echo off
+cd /d %~dp0
+set DOWNLOAD_DIR=%USERPROFILE%\Downloads
+set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
+SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\mingw64;%DOWNLOAD_DIR%\mingw64\bin;%DOWNLOAD_DIR%\cmake-3.22.0-rc1-windows-x86_64\bin;%PATH%
+cmake.exe -G"MinGW Makefiles" -DCMAKE_CXX_COMPILER="%DOWNLOAD_DIR_LINUX%/LLVM-13.0.0-win64/bin/clang++.exe" -DCMAKE_C_COMPILER="%DOWNLOAD_DIR_LINUX%/LLVM-13.0.0-win64/bin/clang.exe" -B./build
+
+cd build
+mingw32-make.exe
+pause
+REM GOTO rebuild_and_startapp
